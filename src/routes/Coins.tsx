494823ef8daa70d091d8c1e35e8093f006165071
SeungHwan-Lee <sh981013s@gmail.com>
2021-12-31 22:11:43 +0900

    replaced fetching func into usage of react-query

--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -63,17 +63,7 @@ interface ICoin {
 
 const Coins = () => {
   const { isLoading, data } = useQuery<ICoin[]>('allCoins', fetchCoins);
-  /*  const [coins, setCoins] = useState<CoinInterface[]>([]);
-  const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    (async () => {
-      const response = await fetch('https://api.coinpaprika.com/v1/coins');
-      const json = await response.json();
-      setCoins(json.slice(0, 100));
-      setLoading(false);
-    })();
-  }, []);*/
   return (
     <Container>
       <Header>
